54538e4287d4def03eafb7f9fd39a89f8c12d811
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2024-01-05 23:58:30 +0300

    add filters to UsersList. remember last filter changes and show cols accordingly.

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,27 +1,54 @@
 import User from "../models/user";
+import {useState, useEffect} from "react";
 
 interface UsersListProps {
   users: User[];
   onRemoveUser: (id: number) => void;
 }
 
+const initial = {
+  id: true,
+  name: true,
+  userName: true,
+  phone: true
+}
+
 function UsersList({ users, onRemoveUser }: UsersListProps) {
+  const [filters, setFilters] = useState(initial);
+
+  const save = () => {
+    localStorage.setItem('filters', JSON.stringify(filters))
+  }
+
+  useEffect(() => {
+    const memory = localStorage.getItem('filters')
+
+    if(!memory) save();
+
+    setFilters(JSON.parse(localStorage.getItem('filters') as string))
+  }, []);
+
   return(
     <>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">id</th>
+            {filters.id && <th scope="col">id</th>}
             <th scope="col">Name</th>
             <th scope="col">User Name</th>
             <th scope="col">phone</th>
+            <th scope="col">
+              <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#filters" aria-controls="filters">
+                Filter
+              </button>
+            </th>
           </tr>
         </thead>
 
         <tbody>
           { users.map(user =>
               <tr key={user.id}>
-                <th scope="row"> {user.id} </th>
+                { filters.id && <th scope="row"> {user.id} </th> }
                 <td> {user.name} </td>
                 <td> {user.username} </td>
                 <td> {user.phone} </td>
@@ -31,6 +58,38 @@ function UsersList({ users, onRemoveUser }: UsersListProps) {
           }
         </tbody>
       </table>
+
+      <section className='Filters'>
+        <div className="offcanvas offcanvas-end" data-bs-scroll="true" tabIndex="-1" id="filters" aria-labelledby="filters">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasExampleLabel">Choose rows</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <div className="form-check form-switch py-2">
+              <input value={filters.id} onChange={e => setFilters({...filters, id: e.target.checked})} checked={filters.id} className="form-check-input" type="checkbox" role="switch" id="id" />
+              <label className="form-check-label" htmlFor="id">ID</label>
+            </div>
+
+            <div className="form-check form-switch py-2">
+              <input className="form-check-input" type="checkbox" role="switch" id="name" />
+              <label className="form-check-label" htmlFor="name">Name</label>
+            </div>
+
+            <div className="form-check form-switch py-2">
+              <input className="form-check-input" type="checkbox" role="switch" id="userName" />
+              <label className="form-check-label" htmlFor="userName">User Name</label>
+            </div>
+
+            <div className="form-check form-switch py-2">
+              <input className="form-check-input" type="checkbox" role="switch" id="phone" />
+              <label className="form-check-label" htmlFor="phone">Phone</label>
+            </div>
+          </div>
+
+          <button onClick={() => save()} className={'btn btn-success mb-3 mx-4'} data-bs-dismiss="offcanvas">save</button>
+        </div>
+      </section>
     </>
   )
 }
